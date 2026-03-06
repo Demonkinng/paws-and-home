c72f18c62774400f60622d0474aa5acdf9c3840c
Demonkinng <angelchuncho5@gmail.com>
2026-03-05 22:46:10 -0500

    feat: extend PetRepository to support JPA specifications

--- backend/src/main/java/com/adcj/backend/repositories/PetRepository.java
+++ backend/src/main/java/com/adcj/backend/repositories/PetRepository.java
@@ -1,21 +1,13 @@
 package com.adcj.backend.repositories;
 
 import com.adcj.backend.models.Pet;
-import com.adcj.backend.models.enums.AdoptionStatus;
-import com.adcj.backend.models.enums.Gender;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PetRepository extends JpaRepository<Pet, Integer> {
+public interface PetRepository extends JpaRepository<Pet, Integer>, JpaSpecificationExecutor<Pet> {
     Optional<Pet> findByImageId(String imageId);
-
-    List<Pet> findByStatus(AdoptionStatus status);
-
-    List<Pet> findByGender(Gender gender);
-
-    List<Pet> findByBreedContainingIgnoreCase(String breed);
 }
