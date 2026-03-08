bd8e180305d79bc60f7f1bf8788795530c9220cc
Demonkinng <angelchuncho5@gmail.com>
2026-03-07 23:54:41 -0500

    fix: change Pet ID generation strategy to IDENTITY and update StorageService to return a UUID for stored files

--- backend/src/main/java/com/adcj/backend/models/Pet.java
+++ backend/src/main/java/com/adcj/backend/models/Pet.java
@@ -17,7 +17,7 @@ import lombok.*;
 )
 public class Pet {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false)
--- backend/src/main/java/com/adcj/backend/services/StorageService.java
+++ backend/src/main/java/com/adcj/backend/services/StorageService.java
@@ -4,13 +4,15 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.UUID;
+
 @Service //TODO: delete this
 public class StorageService {
     StorageService() {
     }
 
     public String store(MultipartFile file) {
-        return "save";
+        return UUID.randomUUID().toString();
     }
 
     public Resource loadAsResource(String imageId) {
